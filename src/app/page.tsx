ce542422421b6bb038d7976b4ec5e2705b9f6983
Clément De Roberti <cderoberti@gmail.com>
2024-12-03 08:29:02 +0100

    feat: {home} integration page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-import type { Metadata } from 'next';
-import Image from 'next/image';
+'use server';
+
 import * as React from 'react';
+
+import '@/styles/views/pages/home.scss';
+
+import Logo from '~/icons/logo.svg';
 
 export default async function Home() {
   return (
-    <>
-      SAE 501
-    </>
+    <main>
+      <div className='background'></div>
+      <div className='barcade-container'>
+        <div className='barcade-logo'>
+          <Logo />
+        </div>
+        <div className='barcade-buttons-box'>
+          <button className='barcade-buttons'>tequila</button>
+          <button className='barcade-buttons'>margarita</button>
+          <button className='barcade-buttons'>paloma</button>
+          <button className='barcade-buttons'>mojito</button>
+          <button className='barcade-buttons'>aviatioin</button>
+          <button className='barcade-buttons'>paper plane</button>
+        </div>
+        <div className='barcade-footer'>
+          <p>
+            L'abus d'alcool est dangereux pour la santé, à consommer avec
+            modération
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
