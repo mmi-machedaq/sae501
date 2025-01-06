62f8175a6573e90ae498430916958f07271cbbd1
Quentin Macheda <quentin.macheda@outlook.fr>
2025-01-06 08:47:35 +0100

    chore(global): update commit message convention

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,11 +7,11 @@ module.exports = {
       rules: {
         'custom-commit-message-rule': ({ raw }) => {
           const regex =
-            /^(feat|fix|docs|style|refactor|perf|test|chore|build|ci): \{.+\} .+$/;
+            /^(feat|fix|docs|style|refactor|perf|test|chore|build|ci)\([a-zA-Z0-9-_]+\)!?: .+$/;
 
           return [
             regex.test(raw.trim()),
-            `Commit message must follow the format => "type: {context} message"`,
+            `Commit message must follow the format => "type(context): message"`,
           ];
         },
       },
