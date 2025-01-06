824d63e7f6aa8c688de0f45c700cb1c21f05df4f
Quentin Macheda <quentin.macheda@outlook.fr>
2025-01-06 10:25:02 +0100

    chore(global): re-structure the files + update some style

--- src/app/bonus/page.tsx
+++ src/app/bonus/page.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import { MdAutorenew } from 'react-icons/md';
 
 import '@/styles/views/pages/home.scss';
+import '@/styles/views/pages/bonus.scss';
 
 import phrases from '@/data/phrases.json';
 
@@ -25,9 +26,11 @@ export default function Bonus() {
   return (
     <main>
       <div className='brc-background'></div>
-      <div className='brc-filling-container'>
+      <div className='brc-bonus-container'>
         <h2>On relance les cartes :</h2>
-        <h3>{randomPhrase || 'Chargement...'}</h3>
+        <p className='brc-bonus-container__random-sentence'>
+          {randomPhrase || 'Chargement...'}
+        </p>
         <div className='brc-buttons-box'>
           <button className='brc-buttons' onClick={handleClick}>
             <MdAutorenew />
--- src/app/serve-drinks-1/page.tsx
+++ src/app/serve-drinks-1/page.tsx
@@ -2,10 +2,10 @@
 
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
-import { FaGlassMartiniAlt } from 'react-icons/fa';
 import { LiaCocktailSolid } from 'react-icons/lia';
 
 import '@/styles/views/pages/home.scss';
+import '@/styles/views/pages/serve-drinks.scss';
 
 export default function ServeDrinks() {
   const [isCupFilled, setIsCupFilled] = useState(false);
@@ -22,19 +22,20 @@ export default function ServeDrinks() {
     <main>
       <div className='brc-background'></div>
       <div className='brc-filling-container'>
-        <h2>Veuillez placer le verre du joueur 1, pour le remplissage</h2>
         <div className='brc-drink-info'>
           <h2 className='drink-name'>
             <LiaCocktailSolid /> {localStorage.getItem('cocktail')}
           </h2>
           <span>Cocktail choisi</span>
         </div>
+        <p className='brc-filling-container__instructions'>
+          Placez le verre du joueur 1 sous la machine à cocktail, pour procéder
+          au remplissage.
+        </p>
         <div className='brc-buttons-box'>
           <button className='brc-buttons liquide-btn' onClick={handleClick}>
             <div className='water-ctr'>
-              <p>
-                <FaGlassMartiniAlt /> Remplir
-              </p>
+              <p>Remplir</p>
               <div className={`water ${isCupFilled ? 'active' : ''}`}></div>
             </div>
           </button>
