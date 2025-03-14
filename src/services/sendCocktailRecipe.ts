41f66e405f9a9bcb1207347dea49a315e9a3eb0f
Quentin Macheda <quentin.macheda@outlook.fr>
2025-03-14 16:50:53 +0100

    fix(serve-drinks): internal api route issue

--- src/services/sendCocktailRecipe.ts
+++ src/services/sendCocktailRecipe.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export async function sendCocktailRecipe(data: object) {
-  const arduinoUrl = 'http://localhost:3000/api/send-data-to-arduino';
-  const response = await axios.post(arduinoUrl, data, {
+  const apiRoute = '/api/send-data-to-arduino';
+  const response = await axios.post(apiRoute, data, {
     headers: {
       'Content-Type': 'application/json',
     },
